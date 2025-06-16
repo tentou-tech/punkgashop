e5fe31c6c08ab46cd25d5c8fd18c6f4b026260f0
Sunday <phamhoangson99@gmail.com>
2025-06-16 14:44:37 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'standalone',
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
